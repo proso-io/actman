e696d1e606d2509e2b86ee1a1f7352397ddcba08
Shivam <shivam270295@gmail.com>
2019-10-05 14:49:45 +0530

    Update logger.clj

--- src/clj/actman/bi/logger.clj
+++ src/clj/actman/bi/logger.clj
@@ -1,5 +1,23 @@
-(ns actman.bi.logger)
+(ns actman.bi.logger
+  (:require [clojure.tools.logging :as log]))
 
-(defn log
-  [tag [% args]]
-  (println tag args))
+(defn log [& args]
+  (log/info args))
+
+(defn trace [& args]
+  (log/trace args))
+
+(defn debug [& args]
+  (log/debug args))
+
+(defn info [& args]
+  (log/info args))
+
+(defn warn [& args]
+  (log/warn args))
+
+(defn error [& args]
+  (log/error args))
+
+(defn fatal [& args]
+  (log/fatal args))
