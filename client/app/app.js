097418b1f950de2622e267726cbec7d515c6ec2a
saumitra1993 <theapogee2011@gmail.com>
2019-11-17 15:24:03 +0530

    init actman client

--- client/app/app.js
+++ client/app/app.js
@@ -6,48 +6,54 @@
  */
 
 // Needed for redux-saga es6 generator support
-import '@babel/polyfill';
+import "@babel/polyfill";
 
 // Import all the third party stuff
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import history from 'utils/history';
-import 'sanitize.css/sanitize.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { Route, Switch } from "react-router";
+import history from "utils/history";
+import "sanitize.css/sanitize.css";
 
 // Import root app
-import App from 'containers/App';
+import LandingPage from "containers/LandingPage";
 
 // Import Language Provider
-import LanguageProvider from 'containers/LanguageProvider';
+import LanguageProvider from "containers/LanguageProvider";
 
 // Load the favicon and the .htaccess file
 /* eslint-disable import/no-unresolved, import/extensions */
-import '!file-loader?name=[name].[ext]!./images/favicon.ico';
-import 'file-loader?name=.htaccess!./.htaccess';
+import "!file-loader?name=[name].[ext]!./images/favicon.ico";
+import "file-loader?name=.htaccess!./.htaccess";
 /* eslint-enable import/no-unresolved, import/extensions */
 
-import configureStore from './configureStore';
+import configureStore from "./configureStore";
 
 // Import i18n messages
-import { translationMessages } from './i18n';
+import { translationMessages } from "./i18n";
 
 // Create redux store with history
 const initialState = {};
 const store = configureStore(initialState, history);
-const MOUNT_NODE = document.getElementById('app');
+const MOUNT_NODE = document.getElementById("app");
 
 const render = messages => {
   ReactDOM.render(
     <Provider store={store}>
       <LanguageProvider messages={messages}>
         <ConnectedRouter history={history}>
-          <App />
+          <>
+            <Switch>
+              <Route exact path="/" render={() => <LandingPage />} />
+              <Route render={() => <div>Miss</div>} />
+            </Switch>
+          </>
         </ConnectedRouter>
       </LanguageProvider>
     </Provider>,
-    MOUNT_NODE,
+    MOUNT_NODE
   );
 };
 
@@ -55,7 +61,7 @@ if (module.hot) {
   // Hot reloadable React components and translation json files
   // modules.hot.accept does not accept dynamic dependencies,
   // have to be constants at compile-time
-  module.hot.accept(['./i18n', 'containers/App'], () => {
+  module.hot.accept(["./i18n", "containers/App"], () => {
     ReactDOM.unmountComponentAtNode(MOUNT_NODE);
     render(translationMessages);
   });
@@ -64,9 +70,9 @@ if (module.hot) {
 // Chunked polyfill for browsers without Intl support
 if (!window.Intl) {
   new Promise(resolve => {
-    resolve(import('intl'));
+    resolve(import("intl"));
   })
-    .then(() => Promise.all([import('intl/locale-data/jsonp/en.js')]))
+    .then(() => Promise.all([import("intl/locale-data/jsonp/en.js")]))
     .then(() => render(translationMessages))
     .catch(err => {
       throw err;
@@ -78,6 +84,6 @@ if (!window.Intl) {
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
 // we do not want it installed
-if (process.env.NODE_ENV === 'production') {
-  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
+if (process.env.NODE_ENV === "production") {
+  require("offline-plugin/runtime").install(); // eslint-disable-line global-require
 }
---
+++ client/app/containers/LandingPage/selectors.js
@@ -0,0 +1,25 @@
+import { createSelector } from "reselect";
+import { initialState } from "./reducer";
+
+/**
+ * Direct selector to the landingPage state domain
+ */
+
+const selectLandingPageDomain = state => state.landingPage || initialState;
+
+/**
+ * Other specific selectors
+ */
+
+/**
+ * Default selector used by LandingPage
+ */
+
+const makeSelectLandingPage = () =>
+  createSelector(
+    selectLandingPageDomain,
+    substate => substate
+  );
+
+export default makeSelectLandingPage;
+export { selectLandingPageDomain };
