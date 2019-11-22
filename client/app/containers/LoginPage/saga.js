92f386bdac97799c719226c0096de895beee7b31
Shivam <shivam270295@gmail.com>
2019-11-22 22:22:06 +0530

    add current-user api

--- client/app/containers/LoginPage/saga.js
+++ client/app/containers/LoginPage/saga.js
@@ -2,9 +2,7 @@ import { take, call, put, select, takeLatest } from 'redux-saga/effects';
 import { LOGIN_REQUEST_ACTION, LOGIN_ENDPOINT } from './constants';
 import { loginResponseAction } from './actions'
 
-
-export function login(data){
-  console.log(data);
+function loginRequest(data){
   let formData = new FormData();
   formData.append('username', data.username);
   formData.append('password', data.password);
@@ -12,20 +10,32 @@ export function login(data){
     method: "POST",
     body: formData
   });
-  loginPromise.then(response => {
-    //console.log("res", response);//})
+  return loginPromise;
+}
+
+export function* login(data){
+  console.log(data);
+  try {
+    const response = yield call(loginRequest, data);
+    console.log("in saga", response);
     let url = response.url;
     let querystring = url.substring(url.indexOf("?"));
     let urlParams = new URLSearchParams(querystring);
     //console.log("url params", urlParams);
     if(urlParams.get("success") === "true"){
       console.log("Login successful");
-      //yield put(loginResponseAction(true));
+      yield put(loginResponseAction(1));
+      //return true
     } else {
       console.log("Login failed");
-      //yield put(loginResponseAction(false));
+      yield put(loginResponseAction(-1));
+      //return false;
     }
-  })
+    //yield put(loginResponseAction(loginStatus));
+  } catch (err) {
+    console.log(err);
+    yield put(loginResponseAction(-1));
+  }
 }
 
 // Individual exports for testing
--- src/clj/actman/db/access_restrictions.clj
+++ src/clj/actman/db/access_restrictions.clj
@@ -8,7 +8,7 @@
   [:oid sc/Str "Id of Organisation" :req-ro]
   [:ent sc/Str "Entity to enforce restriction on" :req-ro]
   [:opn sc/Str "Operation on the entity" :req-ro]
-  [:addon sc/Str "Id of addon if operation belongs to addon" :req-ro]
+  [:addon sc/Str "Id of addon if operation belongs to addon" :opt]
   [:roles db-utils/USER_TEAM_ROLE_SCHEMA "Array of user roles" :req]
   [:users [sc/Str] "Array of user ids" :opt]
   ])
@@ -16,6 +16,7 @@
 (defn get-user-access-operations
   "Get all accessible operations for user and roles"
   [userid roles]
+  (println "in accress" userid roles)
   (let [
     docs
       (get-docs
