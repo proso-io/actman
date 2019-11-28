7f7cd3a6a9aab0ed971aee74db7af4fcecb73e78
saumitra1993 <theapogee2011@gmail.com>
2019-11-28 09:06:09 +0530

    indexed db error fix

--- client/app/services/FormDataService.js
+++ client/app/services/FormDataService.js
@@ -4,13 +4,39 @@ class FormDataService {
   dbObj = null;
 
   constructor() {
-    let db = indexedDB.open("formData");
-    db.onsuccess = event => {
-      this.dbObj = event.target.result;
-    };
-    db.onerror = function(err) {
-      console.log("Failed to mount IndexedDB. All bets are off.");
-    };
+    this.initializeDB()
+      .then(() => {
+        let db = indexedDB.open("formData");
+
+        db.onsuccess = event => {
+          this.dbObj = event.target.result;
+        };
+        db.onerror = function(err) {
+          console.log("Failed to mount IndexedDB. All bets are off.");
+        };
+      })
+      .catch(err => console.log(err));
+  }
+
+  initializeDB() {
+    return new Promise((resolve, reject) => {
+      var formDataDB = window.indexedDB.open("formData", 1);
+
+      formDataDB.onupgradeneeded = function(event) {
+        var db = event.target.result;
+        if (!db.objectStoreNames.contains("formDataObjStore")) {
+          var formDataObjStore = db.createObjectStore("formDataObjStore", {
+            autoIncrement: true
+          });
+          formDataObjStore.createIndex("id", "id", { unique: true });
+        }
+        resolve();
+      };
+
+      formDataDB.onsuccess = function(event) {
+        resolve();
+      };
+    });
   }
 
   hasPendingUploads() {
